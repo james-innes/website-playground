bf3f1a6139df77753f22c89044d7a158d3d108c7
James.Innes <James.Innes@asos.com>
2020-02-17 11:52:24 +0000

    no this is rickahrd start

---
+++ richard/src/Gallery.jsx
@@ -0,0 +1,26 @@
+/* eslint-disable jsx-a11y/alt-text */
+import React from "react";
+import urls from "./assets/json/urls";
+
+export default function Gallery() {
+  return (
+    <article>
+      <header>
+        <h1>Waldorf Education</h1>
+        <p>7 - 16 years at Ringwood Waldorf School</p>
+      </header>
+
+      <div class="gallery">
+        {[1, 3, 4, 5, 6].map(x => (
+          <img src={`./assets/img/${x}.jpg`} />
+        ))}
+      </div>
+
+      <div class="gallery">
+        {urls.map(x => (
+          <img src={x} />
+        ))}
+      </div>
+    </article>
+  );
+}
