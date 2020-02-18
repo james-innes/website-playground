b2d0fe9914eb80e46b6f8bfc028ea315f2675454
James.Innes <James.Innes@asos.com>
2020-02-18 18:38:24 +0000

    Quite happy

---
+++ mad/src/components/gallery.js
@@ -0,0 +1,19 @@
+import React from "react"
+
+const Gallery = () => {
+    return (
+        <nav>
+            <div className="gallery">
+                <img src={"https://images.unsplash.com/photo-1494256997604-768d1f608cac?ixlib=rb-1.2.1&auto=format&fit=crop&w=1701&q=80"} />
+                <img src={"https://images.unsplash.com/photo-1494256997604-768d1f608cac?ixlib=rb-1.2.1&auto=format&fit=crop&w=1701&q=80"} />
+                <img src={"https://images.unsplash.com/photo-1494256997604-768d1f608cac?ixlib=rb-1.2.1&auto=format&fit=crop&w=1701&q=80"} />
+                <img src={"https://images.unsplash.com/photo-1494256997604-768d1f608cac?ixlib=rb-1.2.1&auto=format&fit=crop&w=1701&q=80"} />
+                <img src={"https://images.unsplash.com/photo-1494256997604-768d1f608cac?ixlib=rb-1.2.1&auto=format&fit=crop&w=1701&q=80"} />
+                <img src={"https://images.unsplash.com/photo-1494256997604-768d1f608cac?ixlib=rb-1.2.1&auto=format&fit=crop&w=1701&q=80"} />
+                <img src={"https://images.unsplash.com/photo-1494256997604-768d1f608cac?ixlib=rb-1.2.1&auto=format&fit=crop&w=1701&q=80"} />
+            </div>
+        </nav>
+    )
+}
+
+export default Gallery
---
+++ mad/src/pages/rws.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Layout from "../components/layout"
+import Gallery from "../components/gallery"
+
+export default () => (
+  <Layout>
+    <h1>Waldorf Education</h1>
+    <Gallery />
+  </Layout >
+)
