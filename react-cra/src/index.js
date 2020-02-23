8485fa13f14bde60e2252cd519374d47876ecca9
James Innes <James.Innes@asos.com>
2020-02-23 13:11:18 +0000

    aplollp react typescript

--- react-cra/src/index.js
+++ react-cra/src/index.js
@@ -10,14 +10,12 @@ import ApolloClient from "apollo-boost";
 import { ApolloProvider, useQuery } from "@apollo/react-hooks";
 import gql from "graphql-tag";
 
-import Img from "gatsby-image";
-
 const client = new ApolloClient({
   uri: "http://localhost:8000/___graphql"
 });
 
 function ExchangeRates() {
-  /*  const { loading, error, data } = useQuery(gql`
+  const { loading, error, data } = useQuery(gql`
     {
       allFile(filter: { relativeDirectory: { eq: "images" } }) {
         edges {
@@ -26,68 +24,15 @@ function ExchangeRates() {
           }
         }
       }
-    } 
-  `); */
-
-  const { loading, error, data } = useQuery(gql`
-    {
-      allFile(filter: { relativeDirectory: { eq: "images" } }) {
-        edges {
-          node {
-            childImageSharp {
-              fluid(maxWidth: 192, maxHeight: 144) {
-                ...GatsbyImageSharpFluid
-              }
-            }
-          }
-        }
-      }
     }
   `);
 
-  // ello ello ello
+  data.allFile.map(edges => console.log(edges));
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error :(</p>;
+  console.log(data.allFile);
 
-  /*   console.log(data.allFile.edges); */
-
-  data.allFile.edges.map(node => console.log(node.node.childImageSharp));
-  /* data.allFile.edges.map(node => console.log(node.node.absolutePath));
-   */
-  console.log(data);
-  /* return <div>nothing yet happy</div>; */
-
-  /*   return data.allFile.edges.map(node => (
-    <div>
-      <p>
-        {node}: {node.childImageSharp.fluid.originalName}
-      </p>
-    </div>
-  )); */
-
-  return (
-    <div className="gallery">
-      {data.allFile.edges.map(edge => (
-        <Img fluid={edge.node.childImageSharp.fluid} />
-      ))}
-    </div>
-  );
+  return <div className="gallery"></div>;
 }
-
-/* data": {
-  "allFile": {
-    "edges": [
-      {
-        "node": {
-          "childImageSharp": {
-            "fluid": {
-              "originalName": "2.jpg"
-            }
-          }
-        }
-      },
-      { */
 
 ReactDOM.render(
   <BrowserRouter>
