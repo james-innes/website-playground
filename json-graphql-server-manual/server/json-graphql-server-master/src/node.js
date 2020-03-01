6416bdc276139104d48153f2222c5396de0d1dde
James Innes <James.Innes@asos.com>
2020-03-01 21:42:17 +0000

    Rmoved and moved a bunch of stuff

--- json-graphql-server-manual/server/json-graphql-server-master/src/node.js
+++
@@ -1,5 +0,0 @@
-import jsonGraphqlExpress from './jsonGraphqlExpress';
-import schemaBuilder from './schemaBuilder';
-
-export const jsonSchemaBuilder = schemaBuilder;
-export default jsonGraphqlExpress;
